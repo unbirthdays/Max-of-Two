e8abdb5d8ea459cf1b9d95f9ee90d0bc05356edd
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-08-25 22:36:06 -0500

    Initial commit

---
+++ maxOfTwoSolution.js
@@ -0,0 +1,20 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if (x > y) {
+  console.log(x)
+} else {
+  console.log(y)
+}
+
+// Solution to additional challenge:
+
+if (x > y) {
+  console.log(x)
+} else if (y > x) {
+  console.log(y)
+} else [
+  console.log('The values are identical.')
+]
