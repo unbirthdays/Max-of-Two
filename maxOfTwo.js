dba2dcf8ee89e379c0e0c8c5fc5f66dedce7d229
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-08-25 22:54:03 -0500

    Challenge complete.

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -3,3 +3,10 @@ let y = 2
 
 // Write your code below this line.
 
+if (x < y) {
+    console.log(y);
+} else if (x > y) {
+    console.log(x);
+} else {
+    console.log("The values are identical.");
+}
